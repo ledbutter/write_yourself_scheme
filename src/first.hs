f464b47e83fbe69b3a6451a1469debf286b37803
Sven Grosen <>
2022-12-27 15:09:16 -0600

    Third exercise

--- src/first.hs
+++ src/first.hs
@@ -10,8 +10,12 @@ main = do
     -- lastName <- return (args !! 1)
     -- putStrLn ("Hello, " ++ firstName ++ " " ++ lastName)
     -- #2
-    num1 <- return (read (args !! 0))
-    num2 <- return (read (args !! 1))
-    sum <- return (num1 + num2)
-    sumDisplay <- return (show (sum))
-    putStrLn ("Sum: " ++ sumDisplay)
+    -- num1 <- return (read (args !! 0))
+    -- num2 <- return (read (args !! 1))
+    -- sum <- return (num1 + num2)
+    -- sumDisplay <- return (show (sum))
+    -- putStrLn ("Sum: " ++ sumDisplay)
+    -- #3
+    putStrLn("Please Enter a Name: ")
+    name <- getLine
+    putStrLn("You entered: " ++ name)
