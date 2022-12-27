62ced7b2e7d4cd147846a2ceeaf3d8e44612285a
Sven Grosen <>
2022-12-27 15:06:21 -0600

    Second exercise

--- src/first.hs
+++ src/first.hs
@@ -5,6 +5,13 @@ main :: IO ()
 main = do
     args <- getArgs
     --original: putStrLn ("Hello, " ++ args !! 0)
-    firstName <- return (args !! 0)
-    lastName <- return (args !! 1)
-    putStrLn ("Hello, " ++ firstName ++ " " ++ lastName)
+    -- #1
+    -- firstName <- return (args !! 0)
+    -- lastName <- return (args !! 1)
+    -- putStrLn ("Hello, " ++ firstName ++ " " ++ lastName)
+    -- #2
+    num1 <- return (read (args !! 0))
+    num2 <- return (read (args !! 1))
+    sum <- return (num1 + num2)
+    sumDisplay <- return (show (sum))
+    putStrLn ("Sum: " ++ sumDisplay)
